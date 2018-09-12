b3f95decb7fa5f143f1d8beac0798707c2c8a293
Afzal Ahmad <securedeveloper@gmail.com>
2018-09-12 23:34:29 +0200

    package file updated

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import * as JSZip from 'jszip';
 import {saveAs} from 'file-saver';
 
 
-var zip = new JSZip();
-zip.file("Hello.txt", "Hello World\n");
-zip.generateAsync({type: "blob"}).then(function (content: any) {
-    saveAs(content, "example.zip");
-});
+export default function downloadExcel(): void {
+    var zip = new JSZip();
+    zip.file("Hello.txt", "Hello World\n");
+    zip.generateAsync({type: "blob"}).then(function (content: any) {
+        saveAs(content, "excelFile.xlsx");
+    });
+}
