23b700bbac13802cfd2dda414b18d4f11a1a6181
Afzal Ahmad <securedeveloper@gmail.com>
2019-01-02 13:43:55 +0100

    JSE [finish] xlsx/xl/FileStyles.

--- src/xlsx/xl/FileStyles.ts
+++ src/xlsx/xl/FileStyles.ts
@@ -1,8 +1,14 @@
-import { DEFAULT_FONT_SIZE, DEFAULT_FONT_NAME, DEFAULT_FONT_COLOR } from "../../api/constants";
-import { DEFAULT_STAND_ALONE, DEFAULT_XML_VERSION, ENCODING_UTF_8, EXTENSION_XML, FILE_STYLES } from "../../api/Internals";
-import { ___JSE_XLSX___File, ___JSE_XLSX___Node } from "../../api/xlsx";
-import { JSEFont, JSExcel, JSEColor, JSECellBorder, JSEBorderType } from "../../Types";
-import { extractFontsFromExcel, hasFontsInExcel, hasBordersInExcel, extractBordersFromExcel } from "../../util/ExcelUtil";
+import {DEFAULT_FONT_SIZE, DEFAULT_FONT_NAME, DEFAULT_FONT_COLOR} from "../../api/constants";
+import {
+    DEFAULT_STAND_ALONE,
+    DEFAULT_XML_VERSION,
+    ENCODING_UTF_8,
+    EXTENSION_XML,
+    FILE_STYLES, XMLNS_MAIN, XMLNS_MC, XMLNS_X14AC, XMLNS_X16R2, XMLNS_XR
+} from "../../api/Internals";
+import {___JSE_XLSX___File, ___JSE_XLSX___Node} from "../../api/xlsx";
+import {JSEFont, JSExcel, JSEColor, JSECellBorder, JSEBorderType} from "../../Types";
+import {extractFontsFromExcel, hasFontsInExcel, hasBordersInExcel, extractBordersFromExcel} from "../../util/ExcelUtil";
 
 const fileProps: any = {
     xml: {
@@ -36,6 +42,7 @@ const fileProps: any = {
         bottom: "bottom",
         diagonal: "diagonal",
         cellStyleXfs: "cellStyleXfs",
+        cellXfs: "cellXfs",
         xf: "xf",
         alignment: "alignment",
         cellStyles: "cellStyles",
@@ -43,7 +50,9 @@ const fileProps: any = {
         dxfs: "dxfs",
         tableStyles: "tableStyles",
         extLst: "extLst",
-        ext: "ext"
+        ext: "ext",
+        x14SlicerStyles: "x14:slicerStyles",
+        x15TimelineStyles: "x15:timelineStyles"
     },
     keys: {
         val: "val",
@@ -68,9 +77,16 @@ const fileProps: any = {
         name: "name",
         builtinId: "builtinId",
         defaultTableStyle: "defaultTableStyle",
+        defaultPivotStyle: "defaultPivotStyle",
         uri: "uri",
         xmlnsX14: "xmlns:x14",
         xmlnsX15: "xmlns:x15",
+        xmlns: "xmlns",
+        xmlnsMc: "xmlns:mc",
+        mcIgnorable: "mc:Ignorable",
+        xmlnsX14ac: "xmlns:x14ac",
+        xmlnsX16r2: "xmlns:x16r2",
+        xmlnsXr: "xmlns:xr",
         defaultSlicerStyle: "defaultSlicerStyle",
         defaultTimelineStyle: "defaultTimelineStyle"
     },
@@ -80,15 +96,20 @@ export default (excel: JSExcel): ___JSE_XLSX___File => ({
     fileName: fileProps.name,
     fileExtension: fileProps.extension,
     fileContent: {
-        xml: { ...fileProps.xml },
+        xml: {...fileProps.xml},
         content: {
             name: fileProps.nodes.styleSheet,
             values: [
-                //TODO: Fill out nodes
+                {key: fileProps.keys.xmlns, value: XMLNS_MAIN},
+                {key: fileProps.keys.xmlnsMc, value: XMLNS_MC},
+                {key: fileProps.keys.mcIgnorable, value: "x14ac x16r2 xr"}, //TODO: Double check
+                {key: fileProps.keys.xmlnsX14ac, value: XMLNS_X14AC},
+                {key: fileProps.keys.xmlnsX16r2, value: XMLNS_X16R2},
+                {key: fileProps.keys.xmlnsXr, value: XMLNS_XR}
             ],
             content: [
                 getFontsNodes(excel),
-                //getFillsNodes(excel),
+                // getFillsNodes(excel),
                 getBorderNodes(excel),
                 getCellStyleXfsNodes(excel),
                 getCellXfsNodes(excel),
@@ -115,17 +136,27 @@ function extractFonts(excel: JSExcel): Array<___JSE_XLSX___Node> {
 }
 
 function getFontNode(font: JSEFont): ___JSE_XLSX___Node {
+    const fontCalculatedNodes: Array<___JSE_XLSX___Node | undefined> = [
+        getFontProperty_Bold(font),
+        getFontProperty_Italic(font),
+        getFontProperty_Underline(font),
+        getFontProperty_Size(font),
+        getFontProperty_Color(font),
+        getFontProperty_Name(font),
+        getFontProperty_Family(font)
+    ];
+
+    const fontNodes: Array<___JSE_XLSX___Node> = [];
+
+    fontCalculatedNodes.forEach((item: ___JSE_XLSX___Node | undefined) => {
+        if (item !== null || item !== undefined) {
+            fontNodes.push(item as ___JSE_XLSX___Node);
+        }
+    });
+
     return {
         name: fileProps.nodes.font,
-        content: [
-            getFontProperty_Bold(font),
-            getFontProperty_Italic(font),
-            getFontProperty_Underline(font),
-            getFontProperty_Size(font),
-            getFontProperty_Color(font),
-            getFontProperty_Name(font),
-            getFontProperty_Family(font)
-        ]
+        content: fontNodes
     };
 }
 
@@ -164,7 +195,7 @@ function getFontProperty_Size(font: JSEFont): ___JSE_XLSX___Node {
 
     return {
         name: fileProps.nodes.sz,
-        values: [{ key: fileProps.keys.val, value: fontSize }]
+        values: [{key: fileProps.keys.val, value: fontSize}]
     };
 }
 
@@ -184,18 +215,18 @@ function getFontProperty_Color(font: JSEFont): ___JSE_XLSX___Node {
         fontColor = !colour.rgb ? DEFAULT_FONT_COLOR : colour.rgb;
 
         if (colour.theme) {
-            theme = { key: fileProps.keys.theme, value: colour.theme };
+            theme = {key: fileProps.keys.theme, value: colour.theme};
         }
 
         if (colour.tint) {
-            tint = { key: fileProps.keys.tint, value: colour.tint };
+            tint = {key: fileProps.keys.tint, value: colour.tint};
         }
     }
 
     return {
         name: fileProps.nodes.color,
         values: [
-            { key: fileProps.keys.rgb, value: fontColor },
+            {key: fileProps.keys.rgb, value: fontColor},
             ...theme,
             ...tint
         ]
@@ -207,7 +238,7 @@ function getFontProperty_Name(font: JSEFont): ___JSE_XLSX___Node {
 
     return {
         name: fileProps.nodes.name,
-        values: [{ key: fileProps.keys.val, value: fontName }]
+        values: [{key: fileProps.keys.val, value: fontName}]
     };
 }
 
@@ -218,7 +249,7 @@ function getFontProperty_Family(font: JSEFont): ___JSE_XLSX___Node | undefined {
 
     return {
         name: fileProps.nodes.family,
-        values: [{ key: fileProps.keys.val, value: font.family }]
+        values: [{key: fileProps.keys.val, value: font.family}]
     };
 }
 
@@ -236,8 +267,8 @@ function getBorderNodes(excel: JSExcel): ___JSE_XLSX___Node | undefined {
         const borders: Array<JSECellBorder> = extractBordersFromExcel(excel);
 
         return {
-            name: fileProps.node.borders,
-            values: [{ key: fileProps.keys.count, value: borders.length }],
+            name: fileProps.nodes.borders,
+            values: [{key: fileProps.keys.count, value: borders.length}],
             content: borders.map(getBorderNode)
         };
     }
@@ -245,7 +276,7 @@ function getBorderNodes(excel: JSExcel): ___JSE_XLSX___Node | undefined {
 
 function getBorderNode(border: JSECellBorder): ___JSE_XLSX___Node | undefined {
     return {
-        name: fileProps.node.border,
+        name: fileProps.nodes.border,
         content: [
             getLeftCellBorder(border),
             getRightCellBorder(border),
@@ -267,18 +298,18 @@ function getLeftCellBorder(border: JSECellBorder): ___JSE_XLSX___Node | undefine
     if (border.left.color) {
         colorIndex = {
             content: [{
-                name: fileProps.node.color,
+                name: fileProps.nodes.color,
                 values: [
-                    { key: fileProps.keys.indexed, value: border.left.color }
+                    {key: fileProps.keys.indexed, value: border.left.color}
                 ]
             }]
         };
     }
 
     return {
-        name: fileProps.node.left,
+        name: fileProps.nodes.left,
         values: [
-            { key: fileProps.keys.style, value: style }
+            {key: fileProps.keys.style, value: style}
         ],
         ...colorIndex
     };
@@ -295,18 +326,18 @@ function getRightCellBorder(border: JSECellBorder): ___JSE_XLSX___Node | undefin
     if (border.right.color) {
         colorIndex = {
             content: [{
-                name: fileProps.node.color,
+                name: fileProps.nodes.color,
                 values: [
-                    { key: fileProps.keys.indexed, value: border.right.color }
+                    {key: fileProps.keys.indexed, value: border.right.color}
                 ]
             }]
         };
     }
 
     return {
-        name: fileProps.node.right,
+        name: fileProps.nodes.right,
         values: [
-            { key: fileProps.keys.style, value: style }
+            {key: fileProps.keys.style, value: style}
         ],
         ...colorIndex
     };
@@ -323,18 +354,18 @@ function getTopCellBorder(border: JSECellBorder): ___JSE_XLSX___Node | undefined
     if (border.top.color) {
         colorIndex = {
             content: [{
-                name: fileProps.node.color,
+                name: fileProps.nodes.color,
                 values: [
-                    { key: fileProps.keys.indexed, value: border.top.color }
+                    {key: fileProps.keys.indexed, value: border.top.color}
                 ]
             }]
         };
     }
 
     return {
-        name: fileProps.node.top,
+        name: fileProps.nodes.top,
         values: [
-            { key: fileProps.keys.style, value: style }
+            {key: fileProps.keys.style, value: style}
         ],
         ...colorIndex
     };
@@ -351,18 +382,18 @@ function getBottomCellBorder(border: JSECellBorder): ___JSE_XLSX___Node | undefi
     if (border.bottom.color) {
         colorIndex = {
             content: [{
-                name: fileProps.node.color,
+                name: fileProps.nodes.color,
                 values: [
-                    { key: fileProps.keys.indexed, value: border.bottom.color }
+                    {key: fileProps.keys.indexed, value: border.bottom.color}
                 ]
             }]
         };
     }
 
     return {
-        name: fileProps.node.bottom,
+        name: fileProps.nodes.bottom,
         values: [
-            { key: fileProps.keys.style, value: style }
+            {key: fileProps.keys.style, value: style}
         ],
         ...colorIndex
     };
@@ -379,43 +410,139 @@ function getDiagonalCellBorder(border: JSECellBorder): ___JSE_XLSX___Node | unde
     if (border.diagonal.color) {
         colorIndex = {
             content: [{
-                name: fileProps.node.color,
+                name: fileProps.nodes.color,
                 values: [
-                    { key: fileProps.keys.indexed, value: border.diagonal.color }
+                    {key: fileProps.keys.indexed, value: border.diagonal.color}
                 ]
             }]
         };
     }
 
     return {
-        name: fileProps.node.diagonal,
+        name: fileProps.nodes.diagonal,
         values: [
-            { key: fileProps.keys.style, value: style }
+            {key: fileProps.keys.style, value: style}
         ],
         ...colorIndex
     };
 }
 
 function getCellStyleXfsNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.cellStyleXfs,
+        values: [
+            {key: fileProps.keys.count, value: "1"}
+        ],
+        content: {
+            name: fileProps.nodes.xf,
+            values: [
+                {key: fileProps.keys.numFmtId, value: "0"},
+                {key: fileProps.keys.fontId, value: "0"},
+                {key: fileProps.keys.fillId, value: "0"},
+                {key: fileProps.keys.borderId, value: "0"}
+            ]
+        }
+    };
 }
 
 function getCellXfsNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.cellXfs,
+        values: [
+            {key: fileProps.keys.count, value: "1"}
+        ],
+        content: {
+            name: fileProps.nodes.xf,
+            values: [
+                {key: fileProps.keys.numFmtId, value: "0"},
+                {key: fileProps.keys.fontId, value: "0"},
+                {key: fileProps.keys.fillId, value: "0"},
+                {key: fileProps.keys.borderId, value: "0"},
+                {key: fileProps.keys.xfId, value: "0"}
+            ]
+        }
+    };
 }
 
 function getCellStylesNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.cellStyles,
+        values: [
+            {key: fileProps.keys.count, value: "1"}
+        ],
+        content: {
+            name: fileProps.nodes.cellStyle,
+            values: [
+                {key: fileProps.keys.name, value: "Standard"},
+                {key: fileProps.keys.xfId, value: "0"},
+                {key: fileProps.keys.builtinId, value: "0"}
+            ]
+        }
+    };
 }
 
 function getDXFSNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.dxfs,
+        values: [
+            {key: fileProps.keys.count, value: "0"}
+        ]
+    };
 }
 
 function getTableStylesNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.tableStyles,
+        values: [
+            {key: fileProps.keys.count, value: "0"},
+            {key: fileProps.keys.defaultTableStyle, value: "TableStyleMedium2"},
+            {key: fileProps.keys.defaultPivotStyle, value: "PivotStyleMedium9"}
+        ]
+    };
 }
 
 function getExtLstNodes(excel: JSExcel): ___JSE_XLSX___Node {
-
+    // TODO: complete as with excel sheet -CODE02
+    return {
+        name: fileProps.nodes.extLst,
+        content: [
+            {
+                name: fileProps.nodes.ext,
+                values: [
+                    {key: fileProps.keys.uri, value: "{EB79DEF2-80B8-43e5-95BD-54CBDDF9020C}"},
+                    {
+                        key: fileProps.keys.xmlnsX14,
+                        value: "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main"
+                    }
+                ],
+                content: {
+                    name: fileProps.nodes.x14SlicerStyles,
+                    values: [
+                        {key: fileProps.keys.defaultSlicerStyle, value: "SlicerStyleLight1"}
+                    ]
+                }
+            },
+            {
+                name: fileProps.nodes.ext,
+                values: [
+                    {key: fileProps.keys.uri, value: "{9260A510-F301-46a8-8635-F512D64BE5F5}"},
+                    {
+                        key: fileProps.keys.xmlnsX14,
+                        value: "http://schemas.microsoft.com/office/spreadsheetml/2010/11/main"
+                    }
+                ],
+                content: {
+                    name: fileProps.nodes.x15TimelineStyles,
+                    values: [
+                        {key: fileProps.keys.defaultTimelineStyle, value: "SlicerStyleLight1"}
+                    ]
+                }
+            }
+        ]
+    };
 }
